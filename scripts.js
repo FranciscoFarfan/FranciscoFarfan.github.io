2c31a00c13897603974ec050ca1cd383f485ed8d
Francisco Farfan <ffarfanvazquez@gmail.com>
2025-06-06 15:02:44 -0600

    cerrar otras secciones

--- scripts.js
+++ scripts.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("[id^='btn-']").forEach((boton) => {
+  const botones = document.querySelectorAll("[id^='btn-']");
+  
+  botones.forEach((boton) => {
     boton.addEventListener("click", () => {
       const idBase = boton.id.replace("btn-", "");
       const contenido = document.getElementById(`conocimientos-${idBase}`);
-      const visible = contenido.style.display !== "none";
 
-      if (visible) {
-        contenido.style.display = "none";
-        boton.textContent = "Mostrar";
-      } else {
+     
+      botones.forEach((b) => {
+        const base = b.id.replace("btn-", "");
+        const c = document.getElementById(`conocimientos-${base}`);
+        c.style.display = "none";
+        b.textContent = "Mostrar";
+      });
+
+      
+      if (contenido.style.display === "none") {
         contenido.style.display = "block";
         boton.textContent = "Ocultar";
       }
